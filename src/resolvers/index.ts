ff9f19c82bd3df570dfedc1ce4bde536818bc9d7
Shu Tanabe <sfcofc@gmail.com>
2022-05-10 23:09:22 +0900

    [feat]getRestaurant実装
    リファクタリングが必要

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,8 +1,10 @@
 import { Resolvers } from "../types/generated/graphql";
 import * as mutaiton from "./mutation/";
+import * as query from "./query/";
 
 const resolvers: Resolvers = {
   Mutation: mutaiton,
+  Query: query,
 };
 
 export default resolvers;
---
+++ src/resolvers/query/getRestaurants.ts
@@ -0,0 +1,52 @@
+import { MutationResolvers } from "../../types/generated/graphql";
+
+import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
+import { RestaurantDynamoType } from "../../types/models/restaurantModels";
+import { Restaurant } from "../../types/generated/graphql";
+
+const ddb = new DynamoDBClient({
+  endpoint: "http://localhost:8000",
+});
+
+const scanRestaurants = async () => {
+  try {
+    const data = await ddb.send(
+      new ScanCommand({
+        TableName: "Restaurant",
+      })
+    );
+    return data.Items;
+  } catch (err) {
+    return err;
+  }
+};
+
+const convertRestaurant = (
+  dynamoRestaurant: RestaurantDynamoType
+): Restaurant => {
+  const rest: Restaurant = {
+    restaurantId: dynamoRestaurant.RestaurantId.S,
+    restaurantName: dynamoRestaurant.RestaurantName.S,
+    score: Number(dynamoRestaurant.Score.N),
+    description: dynamoRestaurant.Description.S,
+    updatedDate: dynamoRestaurant.UpdatedDate.S,
+    introducer: dynamoRestaurant.Introducer.S,
+  };
+
+  return rest;
+};
+
+export const getRestaurants: MutationResolvers["getRestaurants"] = async (
+  parent,
+  args,
+  context,
+  info
+) => {
+  const res: RestaurantDynamoType[] = await scanRestaurants();
+  let gotRestaurants: Restaurant[] = [];
+  for (let i = 0; i < res.length; i++) {
+    const converted = convertRestaurant(res[i]);
+    gotRestaurants.push(converted);
+  }
+  return gotRestaurants;
+};
